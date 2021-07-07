81833a993caa46bb5e4210ae2b3b0a83d6b04cdd
byoung <youngben@earthlink.net>
2021-07-07 09:39:31 -0700

    image background is working

--- src/game_view.js
+++ src/game_view.js
@@ -76,7 +76,7 @@ export default class CityBuilder {
 
   
   start() {
-    this.drawBackground(this.ctx)
+    // this.drawBackground(this.ctx)
     this.buildingBar = new BuildingBar(
       this.buildingBarCtx, [this.screenWidth, this.screenHeight],
       this.bloomctx
@@ -120,21 +120,24 @@ export default class CityBuilder {
 
   newMouseStuff() {
     document.body.addEventListener("mousedown", e => {
-      let focus;
+      let windowWidth = document.body.clientWidth;
+      console.log("windowWidth :", windowWidth)
       if (this.circleCollision(
-        this.navStructures.base.center, [e.clientX, e.clientY],
+        [this.navStructures.base.center[0] + ((windowWidth - 1000) / 2), this.navStructures.base.center[1]], [e.clientX, e.clientY],
         this.navStructures.base.radius)
         ) {
+          console.log("test");
           this.offsetX = e.clientX - this.navStructures.base.center[0];
           this.offsetY = e.clientY - this.navStructures.base.center[1];
           console.log("bases", this.structures.base)
           document.body.addEventListener("mousemove", this.baseOnMouseMove);
           document.body.addEventListener("mouseup", this.baseOnMouseUp);
           this.structures.base.push(this.navStructures.base.center);
-        } else if (e.clientX > this.navStructures.building.start[0] - 20 &&
-                    e.clientX < this.navStructures.building.start[0] + 20 &&
-                    e.clientY > this.navStructures.building.start[1] - 20 &&
-                    e.clientY < this.navStructures.building.start[1] + 20         
+      } else if (
+        e.clientX > this.navStructures.building.start[0] - 20 + ((windowWidth - 1000) / 2) &&
+        e.clientX < this.navStructures.building.start[0] + 20 + ((windowWidth - 1000) / 2)  &&
+        e.clientY > this.navStructures.building.start[1] - 20 &&
+        e.clientY < this.navStructures.building.start[1] + 20         
         ) {
           console.log("building")
           this.offsetX = e.clientX - this.navStructures.building.start[0];
@@ -143,19 +146,21 @@ export default class CityBuilder {
           document.body.addEventListener("mouseup", this.buildingOnMouseUp)
           console.log("buildings", this.structures.building)        
           this.structures.building.push(this.navStructures.building.start)  
-        } else if (e.clientX > this.navStructures.redRoad.start[0] - 10 &&
-                    e.clientX < this.navStructures.redRoad.end[0] + 10 &&
-                    e.clientY > this.navStructures.redRoad.start[1] - 10 &&
-                    e.clientY < this.navStructures.redRoad.end[1] + 10) {
+        } else if (
+          e.clientX > this.navStructures.redRoad.start[0] - 10 + ((windowWidth - 1000) / 2)  &&
+          e.clientX < this.navStructures.redRoad.end[0] + 10 + ((windowWidth - 1000) / 2)  &&
+          e.clientY > this.navStructures.redRoad.start[1] - 10 &&
+          e.clientY < this.navStructures.redRoad.end[1] + 10) {
           console.log("red road")
           this.drawingRoad = "red";
           this.structures.roadCursor = [e.clientX, e.clientY];
           document.body.addEventListener("mousemove", this.redRoadOnMouseMove1)
           document.body.addEventListener("mousedown", this.redRoadOnMouseDown1)
-        } else if (e.clientX > this.navStructures.greenRoad.start[0] - 10 &&
-                    e.clientX < this.navStructures.greenRoad.end[0] + 10 &&
-                    e.clientY > this.navStructures.greenRoad.start[1] - 10 &&
-                    e.clientY < this.navStructures.greenRoad.end[1] + 10) {
+        } else if (
+          e.clientX > this.navStructures.greenRoad.start[0] - 10 + ((windowWidth - 1000) / 2) &&
+          e.clientX < this.navStructures.greenRoad.end[0] + 10 + ((windowWidth - 1000) / 2)  &&
+          e.clientY > this.navStructures.greenRoad.start[1] - 10 &&
+          e.clientY < this.navStructures.greenRoad.end[1] + 10) {
           console.log("green road")
           this.drawingRoad = "green";
           this.structures.roadCursor = [e.clientX, e.clientY];
@@ -167,12 +172,14 @@ export default class CityBuilder {
 //[this.screenWidth * 14/20 - 10, this.screenHeight * (11/12) - 10], this.screenWidth * 2/20 + 20, 20
 
   greenRoadOnMouseMove1(e) {
-    this.roadCursor = [e.clientX, e.clientY];
+    let windowWidth = document.body.clientWidth;
+    this.roadCursor = [e.clientX - ((windowWidth - 1000) / 2), e.clientY];
   }
 
   greenRoadOnMouseDown1(e) {
+    let windowWidth = document.body.clientWidth;
     console.log("green road mouse down1", this.structures.greenRoad)
-    this.structures.greenRoad.push([[e.clientX, e.clientY], [e.clientX, e.clientY]])
+    this.structures.greenRoad.push([[e.clientX - ((windowWidth - 1000) / 2), e.clientY], [e.clientX - ((windowWidth - 1000) / 2), e.clientY]])
     document.body.removeEventListener("mousemove", this.greenRoadOnMouseMove1);
     document.body.removeEventListener("mousedown", this.greenRoadOnMouseDown1);
     document.body.addEventListener("mousemove", this.greenRoadOnMouseMove2);
@@ -180,9 +187,10 @@ export default class CityBuilder {
   }
 
   greenRoadOnMouseMove2(e) {
+    let windowWidth = document.body.clientWidth;
     console.log(this.structures.greenRoad)
-    this.roadCursor = [e.clientX, e.clientY];
-    this.structures.greenRoad[this.structures.greenRoad.length - 1][1] = [e.clientX, e.clientY];
+    this.roadCursor = [e.clientX - ((windowWidth - 1000) / 2), e.clientY];
+    this.structures.greenRoad[this.structures.greenRoad.length - 1][1] = [e.clientX - ((windowWidth - 1000) / 2), e.clientY];
   }
 
   greenRoadOnMouseDown2(e) {
@@ -190,6 +198,15 @@ export default class CityBuilder {
     this.drawingRoad = false;
     document.body.removeEventListener("mousemove", this.greenRoadOnMouseMove2);
     document.body.removeEventListener("mousedown", this.greenRoadOnMouseDown2);
+    let lastGreenRoad = this.structures.greenRoad[this.structures.greenRoad.length - 1]
+    setInterval(() => {
+      this.structures.people.push({
+        vector: this.getVector(lastGreenRoad[0], lastGreenRoad[1]),
+        end: lastGreenRoad[1],
+        current: lastGreenRoad[0],
+        delete: false
+      })
+    }, 1000);
   }
 
   baseOnMouseMove(e) {
@@ -203,12 +220,14 @@ export default class CityBuilder {
   }
 
   redRoadOnMouseMove1(e) {
-    this.roadCursor = [e.clientX, e.clientY];
+    let windowWidth = document.body.clientWidth;
+    this.roadCursor = [e.clientX - ((windowWidth - 1000) / 2), e.clientY];
   }
 
   redRoadOnMouseDown1(e) {
+    let windowWidth = document.body.clientWidth;
     console.log("red road mouse down1", this.structures.redRoad)
-    this.structures.redRoad.push([[e.clientX, e.clientY], [e.clientX, e.clientY]])
+    this.structures.redRoad.push([[e.clientX - ((windowWidth - 1000) / 2), e.clientY], [e.clientX - ((windowWidth - 1000) / 2), e.clientY]])
     document.body.removeEventListener("mousemove", this.redRoadOnMouseMove1);
     document.body.removeEventListener("mousedown", this.redRoadOnMouseDown1);
     document.body.addEventListener("mousemove", this.redRoadOnMouseMove2);
@@ -216,9 +235,10 @@ export default class CityBuilder {
   }
 
   redRoadOnMouseMove2(e) {
+    let windowWidth = document.body.clientWidth;
     console.log(this.structures.redRoad)
-    this.roadCursor = [e.clientX, e.clientY];
-    this.structures.redRoad[this.structures.redRoad.length - 1][1] = [e.clientX, e.clientY];
+    this.roadCursor = [e.clientX - ((windowWidth - 1000) / 2), e.clientY];
+    this.structures.redRoad[this.structures.redRoad.length - 1][1] = [e.clientX - ((windowWidth - 1000) / 2), e.clientY];
   }
 
   redRoadOnMouseDown2(e) {
@@ -252,7 +272,7 @@ export default class CityBuilder {
     this.dragCtx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     this.bloomctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    this.drawBackground(this.ctx);
+    // this.drawBackground(this.ctx);
     this.firstStructure.drawBase(this.navStructures.base.center);
     this.firstStructure.drawTriangleBase(this.navStructures.building.start);
     this.structures.base.forEach(center => this.firstStructure.drawBase(center));
